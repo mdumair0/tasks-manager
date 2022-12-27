866c7df1a0b0a33c0b0c23b48afe3ac8484ffaf0
unknown <mohd.umair2325@gmail.com>
2022-12-27 15:54:33 +0530

    CORS setting added

--- src/index.js
+++ src/index.js
@@ -6,6 +6,15 @@ const taskRouter = require('./routers/tasks');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+};
+
+app.use(cors(corsOptions));
+
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
